7d8b9bd13b65fcf12b092faca8b0e6e9c4c70c79
Kevin David <git@kevindavid.org>
2021-11-13 11:16:59 -0500

    Run at page end & use the right API call

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var inputs = document.getElementsByTagName('pwordinput');
+var inputs = document.getElementsByClassName('pwordinput');
 
 for (var i = 0, l = inputs.length; i < l; i++) {
   inputs[i].removeAttribute('readonly');
